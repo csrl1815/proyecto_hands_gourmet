f46bbd3c20ad76cbd09f5acf0cfd5fec3fa23e2a
csrl1815 <50893788+csrl1815@users.noreply.github.com>
2022-11-06 20:03:48 -0600

    first commit

---
+++ proyecto_hands_gourmet/src/main/java/proyecto_g7/demo/ProyectoG7Application.java
@@ -0,0 +1,13 @@
+package proyecto_g7.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoG7Application.class, args);
+	}
+
+}
---
+++ proyecto_hands_gourmet/src/test/java/proyecto_g7/demo/ProyectoG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto_g7.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
